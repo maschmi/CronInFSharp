dbc7f435de15ca17e528e27d4e5a2ab6080fbca9
Martin Schmidt <inw@immernurwollen.de>
2019-04-18 19:40:36 +0200

    added test for daily at

--- cron.test/Tests.fs
+++ cron.test/Tests.fs
@@ -26,11 +26,17 @@ let testIntervals = [
 
 let hoursOfDay = [0 .. 23]
 
-let createCommand h =
+let createHourlyCommand h =
     { id = "test"; message = "message"; interval = Hourly h }
  
+let createTestDate y m d h mm s =
+    DateTime(y, m, d, h, mm, s)
+
 let createTestHours h =
-    DateTime(2019, 4, 13, h, 15, 00)
+    createTestDate 2019 4 13 h 15 00
+    
+let createTestDayily d h =
+    createTestDate 2019 04 d h 0 00
 
 let getTestDetail dt c =
         sprintf "command %A at %A" c dt
@@ -42,10 +48,18 @@ let checkExecutionHrly (dt:DateTime) h c =
     | 0 -> testExecute dt c |> assertSome
     | _ -> testExecute dt c |> assertNone
 
+let checkExecutionDailyAt (dt:DateTime) h c =
+    let assertSome = getTestDetail dt c |> assertSomeWithMessage
+    let assertNone = getTestDetail dt c |> assertNoneWithMessage
+    match dt.Hour - h with
+    | 0 -> testExecute dt c |> assertSome
+    | _ -> testExecute dt c |> assertNone
+
 let assertCommandExecution (dt:DateTime) c =        
     match c.interval with
     | Hourly h -> checkExecutionHrly dt h c 
     | Disabled -> testExecute dt c |> assertNoneWithMessage "disabled"
+    | DailyAt h -> checkExecutionDailyAt dt h c
     | _ -> Assert.False(true, "untested interval")
 
 let prepareTestTimeFunction dt =
@@ -59,13 +73,26 @@ let runCommandsForTest (dt:DateTime list) (c:Command list) =
         |> runCommandsOnPreparedFunctions c
 
 [<Fact>]
-let ExecuteHourly () =
-    let commands = testIntervals |> List.map createCommand
+let ExecuteHourly_ExecutesEveryInterval () =
+    let commands = testIntervals |> List.map createHourlyCommand
     let testTimes = hoursOfDay |> List.map createTestHours
     runCommandsForTest testTimes commands       
 
 [<Fact>]
-let ExecuteDisabled () =
+let ExecuteDisabled_DoesNotExecute () =
     let commands = [{ id="test"; message="disabled"; interval=Disabled }]
     let testTimes = hoursOfDay |> List.map createTestHours
     runCommandsForTest testTimes commands
+
+let createDailyCommand h =
+    { id = "daily"; message = "yay"; interval = DailyAt h}
+
+let daysToRun d =
+    d |> List.map createTestDayily
+
+[<Fact>]
+let ExecuteDaily_ExecutesAtGivenHour () =
+    let commands = hoursOfDay |> List.map createDailyCommand
+    let testTimes =  daysToRun [1..30] |> List.map (fun fl -> List.map fl hoursOfDay) |> Seq.concat |> List.ofSeq
+    runCommandsForTest testTimes commands
+    
