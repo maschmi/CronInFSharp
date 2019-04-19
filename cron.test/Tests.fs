0a99f269d09d8e5f37d921e513fbc04ea738a205
Martin Schmidt <inw@immernurwollen.de>
2019-04-19 13:33:13 +0200

    cleaned up some test code

--- cron.test/Tests.fs
+++ cron.test/Tests.fs
@@ -3,20 +3,28 @@ module Tests
 open System
 open Xunit
 open Cron
-open Microsoft.VisualStudio.TestPlatform.CommunicationUtilities
 
 let testExecute (dt:DateTime) c =
     Cron.intervalDue dt c
 
-let assertSomeWithMessage m c =
+let getTestDetail dt c =
+        sprintf "command %A at %A" c dt
+
+let assertSomeWithMessage c m =
     match c with
     | Some(x) -> Assert.True(true)
     | None -> Assert.True(false, "Expected Some(T) but got None " + m)
 
-let assertNoneWithMessage m c =
+let assertNoneWithMessage c m =
     match c with
     | Some(x) -> Assert.False(true, "Expeted None but got Some(T) " + m)
     | None -> Assert.False(false)
+
+let assertSome dt c  =
+    getTestDetail dt c |> assertSomeWithMessage c
+
+let assertNone dt c =
+    getTestDetail dt c |> assertNoneWithMessage c
 
 let testIntervals = [ 
         1
@@ -26,9 +34,19 @@ let testIntervals = [
 
 let hoursOfDay = [0 .. 23]
 
+let randomHour = 
+    let rnd = System.Random()
+    rnd.Next(0,23)
+
 let createHourlyCommand h =
     { id = "test"; message = "message"; interval = Hourly h }
- 
+
+let createDailyCommand h =
+    { id = "daily"; message = "yay"; interval = DailyAt h}
+
+let createWeekdayCommand h wd =
+    { id = "weekdaysAt"; message = "test"; interval = WeekdayAt (wd,h)}
+
 let createTestDate y m d h mm s =
     DateTime(y, m, d, h, mm, s)
 
@@ -38,46 +56,43 @@ let createTestHours h =
 let createTestDayily d h =
     createTestDate 2019 04 d h 0 00
 
-let getTestDetail dt c =
-        sprintf "command %A at %A" c dt
+let daysToRun d =
+    d |> List.map createTestDayily
 
-let checkExecutionHrly (dt:DateTime) h c =
-    let assertSome = getTestDetail dt c |> assertSomeWithMessage
-    let assertNone = getTestDetail dt c |> assertNoneWithMessage
+let createTestTimes maxDay =
+    daysToRun [1..maxDay] |> List.map (fun fl -> List.map fl hoursOfDay) |> Seq.concat |> List.ofSeq
+
+let createCommandForEveryWeekday h =
+    let days = List.map enum<DayOfWeek> [0..6] 
+    List.map (createWeekdayCommand h) days 
+    
+let checkExecutionHrlyInterval (dt:DateTime) h c =
     match dt.Hour % h with
     | 0 -> testExecute dt c |> assertSome
     | _ -> testExecute dt c |> assertNone
 
-let checkExecutionDailyAt (dt:DateTime) h c =
-    let assertSome = getTestDetail dt c |> assertSomeWithMessage
-    let assertNone = getTestDetail dt c |> assertNoneWithMessage
+let checkExecutionAtHour (dt:DateTime) h c =
     match dt.Hour - h with
     | 0 -> testExecute dt c |> assertSome
     | _ -> testExecute dt c |> assertNone
 
-let checkExecutionWeekdays (dt:DateTime) wd h c =  
-     let assertSome = getTestDetail dt c |> assertSomeWithMessage
-     let assertNone = getTestDetail dt c |> assertNoneWithMessage
+let checkExecutionWeekdaysAt (dt:DateTime) wd h c =  
      match dt.DayOfWeek = wd with
-     | true -> checkExecutionDailyAt dt h c
+     | true -> checkExecutionAtHour dt h c
      | false -> testExecute dt c |> assertNone
 
 let assertCommandExecution (dt:DateTime) c =        
     match c.interval with
-    | Hourly h -> checkExecutionHrly dt h c 
-    | Disabled -> testExecute dt c |> assertNoneWithMessage "disabled"
-    | DailyAt h -> checkExecutionDailyAt dt h c
-    | WeekdayAt (wd,h) -> checkExecutionWeekdays dt wd h c    
+    | Hourly h -> checkExecutionHrlyInterval dt h c 
+    | Disabled -> testExecute dt c |> assertNone
+    | DailyAt h -> checkExecutionAtHour dt h c
+    | WeekdayAt (wd,h) -> checkExecutionWeekdaysAt dt wd h c    
 
-let prepareTestTimeFunction dt =
-       List.map assertCommandExecution dt
-
-let runCommandsOnPreparedFunctions c f =
-       List.map(fun fl -> List.map fl c) f
+let listFunCrossList a funlist =
+    List.map (fun f -> List.map f a) funlist
 
 let runCommandsForTest (dt:DateTime list) (c:Command list) =        
-        prepareTestTimeFunction dt
-        |> runCommandsOnPreparedFunctions c
+    List.map assertCommandExecution dt |> listFunCrossList c
 
 [<Fact>]
 let ExecuteHourly_ExecutesEveryInterval () =
@@ -91,32 +106,11 @@ let ExecuteDisabled_DoesNotExecute () =
     let testTimes = hoursOfDay |> List.map createTestHours
     runCommandsForTest testTimes commands
 
-let createDailyCommand h =
-    { id = "daily"; message = "yay"; interval = DailyAt h}
-
-let daysToRun d =
-    d |> List.map createTestDayily
-
-let createTestTimes maxDay =
-    daysToRun [1..maxDay] |> List.map (fun fl -> List.map fl hoursOfDay) |> Seq.concat |> List.ofSeq
-
 [<Fact>]
 let ExecuteDaily_ExecutesAtGivenHour () =
     let commands = hoursOfDay |> List.map createDailyCommand
     let testTimes = createTestTimes 30
     runCommandsForTest testTimes commands
-   
-let createWeekdayCommand h wd =
-    { id = "weekdaysAt"; message = "test"; interval = WeekdayAt (wd,h)}
-
-let createCommandForEveryWeekday h =
-    let days = List.map enum<DayOfWeek> [0..6] 
-    List.map (createWeekdayCommand h) days
-    
-
-let randomHour = 
-    let rnd = System.Random()
-    rnd.Next(0,23)
 
 [<Fact>]
 let ExecuteEveryWeekdayAt_ExecutesAtGivenHour () =
