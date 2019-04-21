851fe90dbff3f82a76a8366e4e1c0b8f8cd4db3d
Martin Schmidt <inw@immernurwollen.de>
2019-04-21 15:38:36 +0200

    added plus for option

--- Infrastructure.fs
+++ Infrastructure.fs
@@ -11,4 +11,14 @@ module Operators =
     let (>=>) switchFunction1 switchFunction2  =        
         switchFunction1 >> (Option.bind switchFunction2)
 
-    
+    //paralellize switches
+    let plusOption addSome switch1 switch2 x =
+        match (switch1 x),(switch2 x) with
+        | Some s1,Some s2 -> Some (addSome s1 s2)
+        | None,Some _ -> None
+        | Some _,None -> None
+        | None,None -> None
+
+    let (&&&) s1 s2 =
+        let addSome c1 c2 = c1
+        plusOption addSome s1 s2
